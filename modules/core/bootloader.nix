4bfe558c35b3923334583f760741f6fcdd2b4593
Balssh <george.bals25@gmail.com>
2024-10-06 23:30:03 +0300

    wc: comeback to systemd boot

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -5,15 +5,15 @@
 }: {
   boot = {
     loader = {
-      systemd-boot.enable = false;
+      systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
-      grub = {
-        enable = true;
-        device = "nodev";
-        configurationLimit = 10;
-        efiSupport = true;
-        theme = inputs.nixos-grub-themes.packages.${pkgs.system}.nixos;
-      };
+      # grub = {
+      #   enable = true;
+      #   device = "nodev";
+      #   configurationLimit = 10;
+      #   efiSupport = true;
+      #   theme = inputs.nixos-grub-themes.packages.${pkgs.system}.nixos;
+      # };
     };
     kernelPackages = pkgs.linuxPackages_latest;
   };
