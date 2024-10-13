6a7227cda719558283a977571c0dbf1faa09aae0
Balssh <george.bals25@gmail.com>
2024-10-13 18:02:32 +0300

    move aliases around

--- modules/home/common/zsh/alias.nix
+++ modules/home/common/zsh/alias.nix
@@ -15,15 +15,12 @@
       cat = "bat";
       diff = "delta";
       less = "bat";
-      py = "python";
-      ipy = "ipython";
       icat = "kitten icat";
       dsize = "du -hs";
       pdf = "tdf";
       open = "xdg-open";
-      space = "ncdu";
+      space = "gdu";
       man = "BAT_THEME='default' batman";
-
       l = "eza --icons  -a --group-directories-first -1"; #EZA_ICON_SPACING=2
       ll = "eza --icons  -a --group-directories-first -1 --no-user --long";
       tree = "eza --icons --tree --group-directories-first";
@@ -37,9 +34,37 @@
       nix-search = "nh search";
       nix-test = "nh os test";
 
-      # python
+      # Python
+      py = "python";
       piv = "python -m venv .venv";
       psv = "source .venv/bin/activate";
+      ipy = "ipython";
+
+      # Git
+      g = "lazygit";
+      gf = "onefetch --number-of-file-churns 0 --no-color-palette";
+      ga = "git add";
+      gaa = "git add --all";
+      gs = "git status";
+      gb = "git branch";
+      gm = "git merge";
+      gd = "git diff";
+      gpl = "git pull";
+      gplo = "git pull origin";
+      gps = "git push";
+      gpso = "git push origin";
+      gpst = "git push --follow-tags";
+      gcl = "git clone";
+      gc = "git commit";
+      gcm = "git commit -m";
+      gcma = "git add --all && git commit -m";
+      gtag = "git tag -ma";
+      gch = "git checkout";
+      gchb = "git checkout -b";
+      glog = "git log --oneline --decorate --graph";
+      glol = "git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset'";
+      glola = "git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset' --all";
+      glols = "git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset' --stat";
     };
   };
 }
