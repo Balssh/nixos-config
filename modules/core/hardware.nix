807f975e806204ac6e1af18185bdc15104fe6faf
Balssh <george.bals25@gmail.com>
2024-10-16 23:07:49 +0300

    update some packages

--- modules/core/hardware.nix
+++ modules/core/hardware.nix
@@ -26,22 +26,6 @@
     bluetooth = {
       enable = true;
       powerOnBoot = true;
-      package = pkgs.bluez.overrideAttrs (finalAttrs: previousAttrs: rec {
-        version = "5.78";
-
-        src = pkgs.fetchurl {
-          url = "mirror://kernel/linux/bluetooth/bluez-${version}.tar.xz";
-          sha256 = "sha256-gw/tGRXF03W43g9eb0X83qDcxf9f+z0x227Q8A1zxeM=";
-        };
-
-        patches = [];
-
-        buildInputs =
-          previousAttrs.buildInputs
-          ++ [
-            pkgs.python3Packages.pygments
-          ];
-      });
     };
   };
   hardware.enableRedistributableFirmware = true;
--- modules/home/personal/packages.nix
+++ modules/home/personal/packages.nix
@@ -6,7 +6,18 @@
   home.packages = with pkgs; [
     ## CLI utility
     ani-cli
-    cliphist # clipboard manager
+
+    # TODO: remove the override once the PR fixing this is in
+    # nixpkgs-unstable
+    (cliphist.overrideAttrs (_old: {
+      src = pkgs.fetchFromGitHub {
+        owner = "sentriz";
+        repo = "cliphist";
+        rev = "c49dcd26168f704324d90d23b9381f39c30572bd";
+        sha256 = "sha256-2mn55DeF8Yxq5jwQAjAcvZAwAg+pZ4BkEitP6S2N0HY=";
+      };
+      vendorHash = "sha256-M5n7/QWQ5POWE4hSCMa0+GOVhEDCOILYqkSYIGoy/l0=";
+    }))
     ffmpeg
     file # Show file information
     imv # image viewer
