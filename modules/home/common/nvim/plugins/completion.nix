1cffaf9d5fe2429fd8d997cbc7d278402f63628a
GeorgeAndreiBals <andrei.bals@intel.com>
2024-11-04 10:50:06 +0200

    update blink

--- modules/home/common/nvim/plugins/completion.nix
+++ modules/home/common/nvim/plugins/completion.nix
@@ -11,15 +11,48 @@
         enable = true;
         settings = {
           keymap = {
-            show = "<C-space>";
-            hide = "<C-e>";
-            accept = "<C-y>";
-            select_prev = "<C-p>";
-            select_next = "<C-n>";
-            show_documentation = "<C-space>";
-            hide_documentation = "<C-space>";
-            scroll_documentation_up = "<C-b>";
-            scroll_documentation_down = "<C-f>";
+            "<C-space>" = [
+              "show"
+              "show_documentation"
+              "hide_documentation"
+            ];
+            "<C-e>" = [ "hide" ];
+            "<C-y>" = [ "select_and_accept" ];
+
+            "<Up>" = [
+              "select_prev"
+              "fallback"
+            ];
+            "<Down>" = [
+              "select_next"
+              "fallback"
+            ];
+            "<C-p>" = [
+              "select_prev"
+              "fallback"
+            ];
+            "<C-n>" = [
+              "select_next"
+              "fallback"
+            ];
+
+            "<C-b>" = [
+              "scroll_documentation_up"
+              "fallback"
+            ];
+            "<C-f>" = [
+              "scroll_documentation_down"
+              "fallback"
+            ];
+
+            "<Tab>" = [
+              "snippet_forward"
+              "fallback"
+            ];
+            "<S-Tab>" = [
+              "snippet_backward"
+              "fallback"
+            ];
           };
           highlight = {
             use_nvim_cmp_as_default = true;
